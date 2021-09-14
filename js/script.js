264cf69a916b94ab9eef94a88d741e6a3ade7881
Jhodavid <jhodavid007@gmail.com>
2021-09-14 00:50:48 -0500

    conocimiento

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 
 let styleCardArgumento = window.getComputedStyle(document.getElementById('cardItems-Id'));
-let cardArgumentoN = document.getElementById('cardItemsHeight-Id');
+let cardArgumentoN = document.getElementById('cardItemsHeight-Id').style;
+cardArgumentoN.cssText = `height: ${styleCardArgumento.height}`;
 
-cardArgumentoN.style.cssText = `height: ${styleCardArgumento.height}`;
+// let conoItem = document.getElementById('conoItem');
+// let infoCono = document.querySelector('.infoCono');
+
+// console.log(infoCono);
+
+// // conoItem.onmouseover = function myFunction(){
+// //     infoCono.style.display = "block";
+// //     infoCono.style.position = "absolute";
+// // }
+
+// conoItem.addEventListener("mouseover", myFunction);
+
+// let conoItemStyle = document.getElementById('cardItemsHeight-Id');
+
+
 
 
